9e251b090f77c866c0a18a791f0f7a1ff9588e16
neilotoole <neilotoole@apache.org>
2023-12-05 06:32:57 -0700

    mpb render delay example

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/neilotoole/mpb-render-delay
+
+go 1.21.4
+
+require github.com/vbauerster/mpb/v8 v8.7.1-0.20231205062852-da3162c67234
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/vbauerster/mpb/v8"
+)
+
+func main() {
+	const delay = time.Second * 5
+	start := time.Now()
+	delayCh := make(chan struct{})
+	time.AfterFunc(delay, func() { close(delayCh) })
+
+	p := mpb.New(mpb.WithRenderDelay(delayCh))
+	bar := p.New(0, mpb.BarStyle(), mpb.BarRemoveOnComplete())
+	bar.Abort(true)
+	bar.Wait() // <-- blocks here until render delay completes
+	p.Wait()
+
+	fmt.Println("Elapsed: ", time.Since(start))
+}
