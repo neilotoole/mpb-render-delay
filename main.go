fe8e59b5ba44fce84db24ba098ec136b8f26e2b7
neilotoole <neilotoole@apache.org>
2023-12-05 19:02:33 -0700

    Render delay working

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"time"
 
+	"sync"
+
 	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
 )
 
 func main() {
@@ -13,11 +16,43 @@ func main() {
 	delayCh := make(chan struct{})
 	time.AfterFunc(delay, func() { close(delayCh) })
 
+	barWg := &sync.WaitGroup{}
+
 	p := mpb.New(mpb.WithAutoRefresh(), mpb.WithRenderDelay(delayCh))
-	bar := p.New(0, mpb.BarStyle(), mpb.BarRemoveOnComplete())
-	bar.Abort(true)
-	bar.Wait() // <-- blocks here until render delay completes
+
+	for i := 0; i < 10; i++ {
+		startBarWithLifespan(fmt.Sprintf("%d: aborts before render delay", i), barWg, p, time.Second)
+	}
+
+	startBarWithLifespan("aborts after render delay", barWg, p, time.Second*10)
+
+	barWg.Wait()
 	p.Wait()
 
 	fmt.Println("Elapsed: ", time.Since(start))
+}
+
+func startBarWithLifespan(name string, wg *sync.WaitGroup, p *mpb.Progress, lifespan time.Duration) {
+	death := time.Now().Add(lifespan)
+	wg.Add(1)
+
+	bar := p.New(0,
+		mpb.BarStyle(),
+		mpb.BarWidth(40),
+		mpb.PrependDecorators(decor.Name(name, decor.WCSyncWidthR)),
+		mpb.AppendDecorators(decor.CurrentNoUnit("%d")),
+		mpb.BarRemoveOnComplete(),
+	)
+	go func() {
+		defer wg.Done()
+		for {
+
+			if time.Now().After(death) {
+				bar.Abort(true)
+				return
+			}
+			bar.IncrBy(1)
+			time.Sleep(time.Millisecond * 100)
+		}
+	}()
 }
